09d2218e7f16c7c5c0edfce63eef94310978a998
akshayarise <akshayawakearise@gmail.com>
2024-01-03 10:15:07 +0530

    how to use Series and DataFrame in Pandas library

--- src/panda.py
+++ src/panda.py
@@ -1,12 +1,12 @@
 # How to use pandas.
 
 import pandas as pd
-import numpy as np
 
 data = ["Akshay", "kumar", 90, 23, 67]
 ser = pd.Series(data)
 print("data: ", data)
 print("Series: \n", ser)
+
 
 df = pd.DataFrame(data)
 print("Data Frame: ", df)
